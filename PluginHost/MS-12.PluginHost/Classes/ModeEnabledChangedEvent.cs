cb1f2385e56e7f4d14f56185550b86ecd92dcb1d
Automatic9045 <ayax.tk@gmail.com>
2021-03-21 23:07:21 +0900

    ISender.OnChangeModeのイベント化

---
+++ PluginHost/MS-12.PluginHost/Classes/ModeEnabledChangedEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TRS.TMS12.Interfaces
+{
+    /// <summary>
+    /// 各モードの有効・無効が変更されたときに発生する <see cref="IPluginHost.ModeEnabledChanged"/> イベントを処理するメソッドを表します。
+    /// </summary>
+    /// <param name="e">イベントデータを格納している <see cref="ModeEnabledChangedEventArgs"/>。</param>
+    public delegate void ModeEnabledChangedEventHandler(ModeEnabledChangedEventArgs e);
+
+    /// <summary>
+    /// <see cref="IPluginHost.ModeEnabledChanged"/> イベントのデータを提供します。
+    /// </summary>
+    public class ModeEnabledChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 有効・無効が変更されたモードを取得します。
+        /// </summary>
+        public Mode TargetMode { get; }
+
+        /// <summary>
+        /// 対象のモードが有効になったかを取得します。
+        /// </summary>
+        public bool IsModeEnabled { get; }
+
+        public ModeEnabledChangedEventArgs(Mode targetMode, bool isModeEnabled)
+        {
+            TargetMode = targetMode;
+            IsModeEnabled = isModeEnabled;
+        }
+    }
+}
---
+++ PluginHost/MS-12.PluginHost/Classes/SendTypeChangedEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TRS.TMS12.Interfaces
+{
+    /// <summary>
+    /// 操作種別が変更されたときに発生する <see cref="IPluginHost.SendTypeChanged"/> イベントを処理するメソッドを表します。
+    /// </summary>
+    /// <param name="e">イベントデータを格納している <see cref="SendTypeChangedEventArgs"/>。</param>
+    public delegate void SendTypeChangedEventHandler(SendTypeChangedEventArgs e);
+
+    /// <summary>
+    /// <see cref="IPluginHost.SendTypeChanged"/> イベントのデータを提供します。
+    /// </summary>
+    public class SendTypeChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 変更後の <see cref="SendTypes"/> を取得します。
+        /// </summary>
+        public SendTypes? SendType { get; }
+
+        public SendTypeChangedEventArgs(SendTypes? newSendType)
+        {
+            SendType = newSendType;
+        }
+    }
+}
