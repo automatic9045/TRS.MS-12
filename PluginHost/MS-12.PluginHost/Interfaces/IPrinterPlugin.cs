190be76f97d0fbf1bcaa756cfc1f260e33ffeb63
Automatic9045 <ayax.tk@gmail.com>
2021-03-21 23:11:14 +0900

    説明の追加、誤字の修正

--- PluginHost/MS-12.PluginHost/Interfaces/IPrinterPlugin.cs
+++ PluginHost/MS-12.PluginHost/Interfaces/IPrinterPlugin.cs
@@ -28,7 +28,7 @@ namespace TRS.TMS12.Interfaces
         string PrinterName { get; }
 
         /// <summary>
-        /// このプリンターで印刷可能な紙幅を取得します。
+        /// このプリンターで印刷可能な紙幅 [mm] を取得します。
         /// </summary>
         int PrintWidth { get; }
 
@@ -47,19 +47,45 @@ namespace TRS.TMS12.Interfaces
         /// 券を印刷します。
         /// </summary>
         /// <param name="tickets">印刷対象の <see cref="TicketBase"/> のリスト。</param>
-        /// <param name="issueingNumber">各発行毎に附番されるユニークな番号。</param>
+        /// <param name="issuingNumber">発券番号。</param>
         /// <param name="onPrint"><see cref="TicketBase"/> の印刷が完了する毎に呼び出すアクション。引数には、印刷が完了した <see cref="TicketBase"/> の <paramref name="tickets"/> におけるインデックスをとります。</param>
         /// <param name="onError">引数には、発生した例外、例外発生時に印刷していた <see cref="TicketBase"/> の <paramref name="tickets"/> におけるインデックスをとります。</param>
-        void Print(List<TicketBase> tickets, int issueingNumber, Action<int> onPrint, Action<Exception, int> onError);
+        void Print(List<TicketBase> tickets, int issuingNumber, Action<int> onPrint, Action<Exception, int> onError);
     }
 
+    /// <summary>
+    /// よく使われる紙幅をメンバーにもつクラスです。
+    /// </summary>
     public static class PrintWidthes
     {
+        /// <summary>
+        /// 58 mm。マルス券と同一幅で、レシート等にも使われます。
+        /// </summary>
         public const int Width58 = 58;
+
+        /// <summary>
+        /// 80 mm。一般的なマルス券の長さ (85 mm) に近い値で、券面を横向きで印刷するのに適しています。レシート等にも使われます。        
+        /// </summary>
         public const int Width80 = 80;
+
+        /// <summary>
+        /// 162 mm。B5 コピー用紙の短辺の長さから両端 10 mm ずつの余白を除いた値です。
+        /// </summary>
         public const int WidthB5Shorter = 182 - 10 * 2;
+
+        /// <summary>
+        /// 237 mm。B5 コピー用紙の長辺の長さから両端 10 mm ずつの余白を除いた値です。
+        /// </summary>
         public const int WidthB5Longer = 257 - 10 * 2;
+
+        /// <summary>
+        /// 190 mm。A4 コピー用紙の短辺の長さから両端 10 mm ずつの余白を除いた値です。
+        /// </summary>
         public const int WidthA4Shorter = 210 - 10 * 2;
+
+        /// <summary>
+        /// 277 mm。A4 コピー用紙の長辺の長さから両端 10 mm ずつの余白を除いた値です。
+        /// </summary>
         public const int WidthA4Longer = 297 - 10 * 2;
     }
 }
