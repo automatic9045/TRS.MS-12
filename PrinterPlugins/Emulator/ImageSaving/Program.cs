3d91147c133f6d8c57efbaf62787a7833a63d2e8
Automatic9045 <ayax.tk@gmail.com>
2021-03-21 13:41:08 +0900

    IPrinterPluginの仕様変更、Ticketの抽象クラス化

--- PrinterPlugins/Emulator/ImageSaving/Program.cs
+++ PrinterPlugins/Emulator/ImageSaving/Program.cs
@@ -12,11 +12,14 @@ using TRS.TMS12.Static;
 
 namespace TRS.TMS12.PrinterPlugins.Emulators.ImageSaving
 {
-    public class ImageSaving : IPrinterPlugin
+    public class Printer58 : IPrinterPlugin
     {
-        public string PrinterName { get; } = "Image-Saving Printer Emulator";
+        public string PrinterName { get; } = "Image-Saving Printer Emulator W58mm";
+        public int PrintWidth { get; } = PrintWidthes.Width58;
 
         public IPluginHost PluginHost { get; set; }
+
+        private readonly string processCode = DateTime.Now.ToString().Replace("/", "").Replace(":", "").Replace(" ", "-");
 
         public void Initialize(string printerName)
         {
@@ -26,28 +29,21 @@ namespace TRS.TMS12.PrinterPlugins.Emulators.ImageSaving
         {
         }
 
-        public void Print(List<Ticket> tickets, Action<int> onPrint, Action<Exception, int> onError, bool isStart, bool isEnd)
+        public void Print(List<TicketBase> tickets, int issueingNumber, Action<int> onPrint, Action<Exception, int> onError)
         {
             tickets.ForEach((t, i) =>
             {
                 try
                 {
-                    Bitmap bmp = new Bitmap(t.Bitmap);
-                    using (Graphics g = Graphics.FromImage(bmp))
-                    {
-                        g.DrawLine(Pens.Red, 0, 0, bmp.Width, bmp.Height);
-                        g.DrawLine(Pens.Red, 0, bmp.Height, bmp.Width, 0);
-                    }
-
-                    Directory.CreateDirectory("Images");
-                    bmp.Save($"Images\\{DateTime.Now.ToString().Replace("/", "").Replace(":", "").Replace(" ", "-")}.bmp", ImageFormat.Bmp);
-
-                    onPrint(i);
+                    Directory.CreateDirectory($"Images\\{processCode}");
+                    t.Bitmap.Save($"Images\\{processCode}\\{string.Format("{0:D5}", issueingNumber)}-{string.Format("{0:D2}", i + 1)}.bmp", ImageFormat.Bmp);
                 }
                 catch (Exception ex)
                 {
                     onError(ex, i);
                 }
+
+                onPrint(i);
             });
         }
     }
