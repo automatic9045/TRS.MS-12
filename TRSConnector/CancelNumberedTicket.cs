627363cce3a72bb9589152428ce2fc22625a4de5
Automatic9045 <67314487+Automatic9045@users.noreply.github.com>
2021-03-29 01:10:05 +0900

    整理券控除機能の追加

---
+++ TRSConnector/CancelNumberedTicket.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+using TRS.TMS12.Interfaces;
+using TRS.Tickets;
+using NativeNumberedTicket = TRS.Tickets.NumberedTicket;
+
+namespace TRS.TMS12.Plugins.TRS
+{
+    public partial class Connector : IPlugin
+    {
+        public SendResult CancelNumberedTicket(Game game, int timeCode, int seatCode, int cNumber)
+        {
+            int number = (int)game * 100 + timeCode;
+
+            dynamic json = null;
+            SendResult result = null;
+            switch (PluginHost.SendType)
+            {
+                case SendTypes.Inquire:
+                    try
+                    {
+                        json = Communicator.Inquire(number);
+                    }
+                    catch (Exception ex)
+                    {
+                        return SendResult.Error(ex);
+                    }
+
+                    try
+                    {
+                        result = ParseResult(json);
+                    }
+                    catch (Exception ex)
+                    {
+                        try
+                        {
+                            return SendResult.Error(ex, json, ResultTypeStringToEnum(json));
+                        }
+                        catch
+                        {
+                            return SendResult.Error(ex);
+                        }
+                    }
+                    break;
+
+                case SendTypes.Sell:
+                case SendTypes.Reserve:
+                    try
+                    {
+                        json = Communicator.Cancel((int)game * 10000 + timeCode * 100 + seatCode, cNumber);
+                    }
+                    catch (Exception ex)
+                    {
+                        return SendResult.Error(ex);
+                    }
+
+                    try
+                    {
+                        switch ((string)json.result)
+                        {
+                            case "yes":
+                                result = SendResult.Yes("", "", false);
+                                break;
+
+                            case "notReserved":
+                                result = SendResult.No("指定された枠は予約されていません");
+                                break;
+
+                            case "used":
+                                result = SendResult.No("使用済");
+                                break;
+
+                            case "wrongCNumber":
+                                result = SendResult.Rethink("Ｃ符号誤り");
+                                break;
+
+                            case "invalidValue":
+                                result = SendResult.Rethink("コード誤り");
+                                break;
+
+                            default:
+                                result = SendResult.Rethink((string)json.result);
+                                break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return SendResult.Error(ex);
+                    }
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TicketPlugins/TRS/NumberedTickets/Cancel/Sender.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.VisualBasic;
+
+using Prism.Mvvm;
+using Prism.Commands;
+
+using Gayak.Collections;
+
+using TRS.TMS12.Static;
+using TRS.TMS12.Interfaces;
+using TRS.TMS12.Resources;
+using TRS.TMS12.Plugins.TRS;
+using static TRS.TMS12.Static.App;
+
+namespace TRS.TMS12.TicketPlugins.TRSTickets.NumberedTickets.Cancel
+{
+    public class Sender : ISender
+    {
+        private PluginInfo m;
+
+        private Connector _Connector = null;
+        private Connector Connector
+        {
+            get
+            {
+                if (_Connector is null)
+                {
+                    _Connector = (Connector)m.PluginHost.Plugins.Find(p => p is Connector);
+                }
+
+                return _Connector;
+            }
+        }
+
+        public Sender(PluginInfo m)
+        {
+            this.m = m;
+        }
+
+        public SendResult Send()
+        {
+            string value = m.TextBoxes[(int)InputControlTextBox.Service];
+            Game game = value switch
+            {
+                "トレインシミュレーター" => Game.TrainSimulator,
+                "模型体験運転" => Game.ModelTrainDriving,
+                _ => (Game)WideStringToInt(value),
+            };
+
+            int timeCode = WideStringToInt(m.TextBoxes[(int)InputControlTextBox.TimeCode]);
+            int seatCode = WideStringToInt(m.TextBoxes[(int)InputControlTextBox.SeatCode]);
+            int cNumber = WideStringToInt(m.TextBoxes[(int)InputControlTextBox.CNumber]);
+
+            SendResult result = Connector.CancelNumberedTicket(game, timeCode, seatCode, cNumber);
+            return result;
+        }
+
+        public List<TimeInquiringInfo> InquireTimeNumbers(Game game)
+        {
+            return Connector.InquireTimeNumbers(game);
+        }
+    }
+}
