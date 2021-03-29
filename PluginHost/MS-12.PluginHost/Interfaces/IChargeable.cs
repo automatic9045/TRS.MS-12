28ea96b36807212b477aba0dd36ce0957eb6b119
Automatic9045 <67314487+Automatic9045@users.noreply.github.com>
2021-03-29 15:20:06 +0900

    IChargeableインターフェイスを追加

---
+++ PluginHost/MS-12.PluginHost/Interfaces/IChargeable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TRS.TMS12.Interfaces
+{
+    /// <summary>
+    /// クラスに金額を定義します。
+    /// </summary>
+    public interface IChargeable
+    {
+        /// <summary>
+        /// 金額を取得します。
+        /// </summary>
+        int Amount { get; }
+    }
+}
